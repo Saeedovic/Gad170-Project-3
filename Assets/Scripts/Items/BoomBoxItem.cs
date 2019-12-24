a9a90202c42838f9ddc58dfa937ab2a94ca332a9
saeed <seedxlss@gmail.com>
2019-12-24 19:27:48 +0300

    Everything

---
+++ Assets/Scripts/Items/BoomBoxItem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item that when used changes to the next song, when out of songs turns off, when used while off, plays first song.
+/// 
+/// TODO; It should auto play, randomise order potentially and go to next track when used.
+///     In other words, act kind of like the radio in a GTA style game.
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class BoomBoxItem : InteractiveItem
+{
+    //TODO: you will need more data than this, like clips to play and a way to know which clip is playing
+    public  AudioSource audioSource;
+
+    int i = 0;
+
+    public AudioClip[] songs;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        audioSource.loop = true;
+        
+
+
+
+    }
+
+    
+    public void PlayClip()
+    {
+        //TODO; this is where you might want to setup and ensure the desire clip is playing on the source
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+        audioSource.clip = songs[Random.Range(0, songs.Length)];
+        audioSource.Play();
+        i++;
+
+
+        if (i == songs.Length - 1)
+            i = 0;
+
+        //TODO; this where we need to go to next track and start and stop playing
+    }
+}
---
+++ Assets/Scripts/Items/NerfGunItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Item that when used while held acts as a physics based projectile instantiator
+/// </summary>
+public class NerfGunItem : InteractiveItem
+{
+    public GameObject nerfDartPrefab;
+    public Transform nerfDartSpawnLocation;
+    public float fireRate = 1;
+    public float launchForce = 10;
+    protected float fireRateCounter;
+
+
+    GameObject bullet;
+
+    protected void Update()
+    {
+    }
+
+    public override void OnUse()
+    {
+        base.OnUse();
+        
+
+        bullet = Instantiate(nerfDartPrefab,nerfDartSpawnLocation.position,Quaternion.identity);
+
+        FireNow();
+
+
+        //TODO: we need to determine if we can fire and if so, make the thing
+    }
+
+    public void FireNow()
+    {
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * launchForce);
+        //TODO: this is where we would actually create the thing and get it on its way
+    }
+}
